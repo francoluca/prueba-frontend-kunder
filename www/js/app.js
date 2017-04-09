571e5c6ca55877858a543d107309a13e672305b6
francoluca <franluca15@gmail.com>
2017-04-09 05:58:37 -0400

    Información del Cómic
    Se abre una nueva ventana cuando se clickea un cómic de la lista, ésta
    muestra mayor información del cómic.
    Se cambian los estilos (Diseño).

--- www/js/app.js
+++ www/js/app.js
@@ -4,60 +4,95 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 (function(){
-      var app = angular.module('starter', ['ionic', 'angular-md5', 'angularMoment']);
+  var app = angular.module('starter', ['ionic', 'angular-md5', 'angularMoment']);
 
-      app.controller('MarvelController', function($scope,$http,md5){
-          $scope.comics = [];
-          var publicKey = 'eda41fddfede76ce5e93da44207bb312';
-          var privateKey = '86cb34221752d5df1c0a70554c619d29d492e417';
-          var ts = Date.now();
+  app.controller('MarvelController', function($scope,$http,md5,$state){
+    
+    $scope.comics = [];
+    $scope.itemComic =$state.params.comicId
+    var publicKey = 'eda41fddfede76ce5e93da44207bb312';
+    var privateKey = '86cb34221752d5df1c0a70554c619d29d492e417';
+    var ts = Date.now();
 
-          var hash = md5.createHash(ts+privateKey+publicKey);
-          var baseUrl = 'https://gateway.marvel.com/v1/';
+    var hash = md5.createHash(ts+privateKey+publicKey);
+    var baseUrl = 'https://gateway.marvel.com/v1/';
           
-          //Se conecta por medio de GET a la API Marvel, es necesario timestamp, apikey y hash, para obtener permisos.
-          $http.get(baseUrl + 'public/comics', {
-              params: {
-                ts: ts,
-                apikey: publicKey,
-                hash: hash,
-                limit: 100
-              }
-            }).success(function(comics){
+    //Se conecta por medio de GET a la API Marvel, es necesario timestamp, apikey y hash, para obtener permisos.
+    $http.get(baseUrl + 'public/comics', {
+      params: {
+        ts: ts,
+        apikey: publicKey,
+        hash: hash,
+        limit: 100
+      }
+    }).success(function(comics){
               
-              /*Se leen los datos desde API Marvel y se extrae id,title,thumbnail,year de cada comic
-              luego se guardan en un arreglo. */
-              angular.forEach(comics.data.results, function(comic){
-                  $scope.comics.push(
-                    {
-                      "id": comic.id,
-                      "title": comic.title,
-                      "thumbnail": comic.thumbnail.path+'.'+comic.thumbnail.extension,
-                      "year": moment(comic.dates[0].date).format("YYYY"),
-                      "description": comic.description
-                    });
-              })
-
+      /*Se leen los datos desde API Marvel y se extrae id,title,thumbnail,year de cada comic
+      luego se guardan en un arreglo. */
+      angular.forEach(comics.data.results, function(comic){
+        $scope.comics.push(
+          {
+            "id": comic.id,
+            "title": comic.title,
+            "thumbnail": comic.thumbnail.path+'.'+comic.thumbnail.extension,
+            "year": moment(comic.dates[0].date).format("YYYY"),
+            "description": comic.description,
+            "images": comic.images,
+            "creators": comic.creators,
+            "characters": comic.characters
           });
-      });
+      })
 
-      
+    });
+  });
 
-    app.run(function($ionicPlatform) {
-      $ionicPlatform.ready(function() {
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-          // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-          // for form inputs)
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  app.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('tabs',{
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
 
-          // Don't remove this line unless you know what you are doing. It stops the viewport
-          // from snapping when text inputs are focused. Ionic handles this internally for
-          // a much nicer keyboard experience.
-          cordova.plugins.Keyboard.disableScroll(true);
+      .state('tabs.lista',{
+        url: '/lista',
+        views: {
+          'lista-tab': {
+            templateUrl: 'templates/lista.html',
+            controller: 'MarvelController'
+          }
         }
-        if(window.StatusBar) {
-          StatusBar.styleDefault();
+      })
+
+      .state('tabs.comic',{
+        url: '/lista/:comicId',
+        views: {
+          'lista-tab': {
+            templateUrl: 'templates/comic.html',
+            controller: 'MarvelController'
+          }
         }
-      });
-    })
+      })
+
+
+      $urlRouterProvider.otherwise('/tab/lista');
+  });    
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
 }());
